91cba49ba641b13fe8b1e40f7606836fb3ceccfe
acoulton <andrew@ingenerator.com>
2026-09-12 12:36:57 +0100

    test: Don't call `getScenarios` to get Feature children in tests
    Tests should use the new `getExecutableChildren` unless they are
    specifically testing the behaviour of hoisting Rule scenarios for BC.

--- tests/Filter/TagFilterTest.php
+++ tests/Filter/TagFilterTest.php
@@ -941,7 +941,7 @@ class TagFilterTest extends FilterTestCase
                   | anything  | 
             GHERKIN,
         );
-        $outline = $feature->getScenarios()[0];
+        $outline = $feature->getExecutableChildren()[0];
         $this->assertInstanceOf(OutlineNode::class, $outline);
         $example = $outline->getExamples()[0];
 
